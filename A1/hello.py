2e37f0793f7b4cae5e3a928b4447d247dec83bfa
adriandomcoll <adrian.dom.coll@gmail.com>
2025-09-22 14:54:57 +0200

    v1

---
+++ A1/hello.py
@@ -0,0 +1,15 @@
+a=5
+b=2.2
+s ="Hola"
+
+print(type(a))
+print(type(b))
+print(type(s))
+
+if a == 5: 
+    print ("es iguallll")
+    print ("asi es, es iguallll")
+
+
+print ("fuera del if ya")
+
