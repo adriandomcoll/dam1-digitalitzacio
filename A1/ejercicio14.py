1d473ac4a9d1ff5decd63875f6ca3ced6b166404
adriandomcoll <adrian.dom.coll@gmail.com>
2025-10-20 14:14:08 +0200

    20/10/2025

--- A1/ejercicio14.py
+++ A1/ejercicio14.py
@@ -2,6 +2,6 @@ paraula = input("Escriu una paraula: ")
 inversa = ""
 
 for lletra in paraula:
-    inversa = lletra + inversa   # vas construint la cadena al revés
+    inversa = lletra + inversa   # Construye la cadena al revés
 
 print("La paraula invertida és:", inversa) 
