5413f1465069ddf7adfd0b568ff826238fee9cc1
adriandomcoll <adrian.dom.coll@gmail.com>
2025-10-20 14:56:41 +0200

    20/10/2025b

---
+++ A1.2/ejercicio5.py
@@ -0,0 +1,9 @@
+lista_compra = ["pomes", "pa", "oli", "llet"]
+for element in lista_compra:
+    print("He de comprar "+ element)
+
+
+cont=0
+while cont < len(lista_compra):
+    print("He de comprar "+lista_compra[cont])
+    cont+=1
