ec40c8f1138d17e2b2499844110444fdfa86b102
adriandomcoll <adrian.dom.coll@gmail.com>
2026-01-13 08:37:50 +0100

    13/01/2026

--- TapatApp/prototip1/server.py
+++ TapatApp/prototip1/server.py
@@ -2,36 +2,34 @@ from flask import Flask, request, jsonify
 
 
 class User:
-    def __init__(self,username, nom, password, email, rol):
-        self.username=username
-        self.nom=nom
-        self.password=password
-        self.email=email
-        self.rol=rol
+    def __init__(self, username, nom, password, email, rol):
+        self.username = username
+        self.nom = nom
+        self.password = password
+        self.email = email
+        self.rol = rol
 
     def __str__(self):
         return self.nom
 
 
-#us1=User(username="addo",nom="Adri",password="1234",email="ado@test.es",rol="ADMIN")
-
 users = [
-    User(username="addo",nom="Adri",password="1234",email="ado@test.es",rol="ADMIN"),
-    User(username="jodo",nom="John",password="1234",email="jodo@test.es",rol="ADMIN"),
-    User(username="maria",nom="Maria",password="1234",email="madb@test.es",rol="ADMIN"),
-    User(username="jodo",nom="John",password="1234",email="jodo@test.es",rol="ADMIN")
+    User("addo", "Adri", "1234", "ado@test.es", "ADMIN"),
+    User("jodo", "John", "1234", "jodo@test.es", "ADMIN"),
+    User("maria", "Maria", "1234", "madb@test.es", "ADMIN"),
+    User("jodo", "John", "1234", "jodo@test.es", "ADMIN")
 ]
+
 
 class UserDao:
     def __init__(self):
-        self.users=users
+        self.users = users
         
-    def getUserByUsername(self,uname):
-        user = None
+    def getUserByUsername(self, uname):
         for u in self.users:
             if u.username == uname:
-                user = u.__dict__
-        return user
+                return u.__dict__
+        return None
     
     def getAllUsers(self):
         resposta = []
@@ -39,48 +37,48 @@ class UserDao:
             resposta.append(u.__dict__)
         return resposta
 
-def getAllUsers(self):
-    return [u.to_dict() for u in self.users]
+    def addUser(self, user):
+        self.users.append(user)
+
 
 user_dao = UserDao()
 app = Flask(__name__)
 
-def __init__(self):
-    self.users = users
 
-
-def addUser(self, user):
-    self.users.append(user)
-
-
-@app.route('/user',methods=['GET'])
+@app.route('/user', methods=['GET'])
 def user():
-    resposta=""
-    # Parametres
-    username = request.args.get("username",default="")
-    # Si els paràmetres OK
-    if username != "":     
-        # Ir al DAO Server y buscar User por username 
-        resposta=user_dao.getUserByUsername(username)
-        if resposta == None:
-            resposta = {"msg":"Usuari No Trobat"}
-
-    # Si els paràmetres NO OK
-    else: 
-        # respondre error 
-        resposta={"msg":"Falta paràmetre Username"}
+    username = request.args.get("username", default="")
+    
+    if username != "":
+        resposta = user_dao.getUserByUsername(username)
+        if resposta is None:
+            resposta = {"msg": "Usuari No Trobat"}
+    else:
+        resposta = {"msg": "Falta paràmetre Username"}
     
     return jsonify(resposta)
 
+
 @app.route('/getuser', methods=['GET'])
-def userList(self):
-    resposta = user_dao.get
+def userList():
     return jsonify(user_dao.getAllUsers())
 
+
 @app.route('/users', methods=['POST'])
-def addUser(self):
-    self.users.append(user)
-    return user
+def addUser():
+    data = request.get_json()
+
+    user = User(
+        data["username"],
+        data["nom"],
+        data["password"],
+        data["email"],
+        data["rol"]
+    )
+
+    user_dao.addUser(user)
+    return jsonify(user.__dict__), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
