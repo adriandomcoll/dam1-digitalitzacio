d1176e8560116295ba728f7c0ad63b8544ce9e8a
adriandomcoll <adrian.dom.coll@gmail.com>
2026-03-02 14:56:06 +0100

    02/03/2026

---
+++ TapatApp/A2/prototip2/View.py
@@ -0,0 +1,35 @@
+
+
+class ViewConsole:
+
+    def viewShowmenu(self):
+        print("1: Login")
+        print("2: Quit")
+        while(True):
+            option=input("Enter Option: ")
+            if(option.isdigit()):
+                optionInt=int(option)
+                if (option >= 1 and option <= 2):
+                    return optionInt
+               
+            print("ERROR: Introduce un valor correcto")
+
+    def viewGeneral(self):
+        option=-1
+        while(option!=2):
+            option=self.viewShowmenu()
+            match option:
+                case 1:
+                    self.viewLogin()
+                    #login
+                case 2:
+                    #Quit
+                    print("Saliendo de la aplicación...")
+
+    def viewLogin(self):
+        print("View LOGIN")
+        print("Introduce el username/email i el password")
+        username=input("Username o email: ")
+        passwd=input("Password: ")
+        # DaoUserClient ha de fer login
+        # Depenent de la resposta va a ser child o User not Autheticated
