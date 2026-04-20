ec72ae818f29e8f8af0d4dc49c1fe713d5da4b89
adriandomcoll <adrian.dom.coll@gmail.com>
2026-04-20 15:00:56 +0200

    20/04/2026

--- TapatApp/prototip3/server/DaoServer.py
+++ TapatApp/prototip3/server/DaoServer.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass, asdict
 from flask import jsonify
+from time import time
 import mysql.connector
+import hashlib
+import random
 
 class UserDao:
 
@@ -25,12 +28,42 @@ class UserDao:
         user = cursor.fetchone()
         cursor.close()
         con.close()
-        # Query para validar Usuario 
-            # if return 1 = registro User OK
-            # else None
-        # Cerrar conexión
         return user
+    def setTokenUser (self, username):
+        # conectar a BBDD
+        con=self.connectBBDD()
+        cursor = con.cursor(dictionary=True)
+        # generar Token
+        token=self.getHash(username)
+
+        # Update a BBDD camp token al usuari per username
+        query = "UPDATE User SET token ='"+token +"' WHERE username" + username
+        print(query)
+        cursor.execute(query)
+        # close BBDD
+        cursor.close()
+        con.close()
     
+    def getHash(self, username):
+        miliseconds = str(time() * 1000)
+        data = username + miliseconds
+        hash_object = hashlib.sha256(data.encode('utf-8'))
+        return hash_object.hexdigest()
+
 dao = UserDao()
+print(dao.getHash("user1"))
+
 u = dao.login("mare", "mare")
 print(u)
+
+
+miliseconds = str(time() * 1000)
+print("Time in milliseconds since epoch", miliseconds)
+
+data = "Holas " + miliseconds
+print(data)
+# Crea el objeto SHA-256
+
+# Obtener el resultado en formato hexadecimal 
+# token = hash_object.hexdigest()
+# print(token)
